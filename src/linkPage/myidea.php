6e6e44eaf68d4a994a7c564854f269a48a0c8ba6
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-11-22 12:11:42 +0900

    myidea[01]

--- src/linkPage/myidea.php
+++ src/linkPage/myidea.php
@@ -7,8 +7,13 @@ require_once __DIR__ . '/../header.php';
 ?>
 
 <!--自分の出したアイデアを表示させるページ-->
-<!-- アイデアを出すボタン -->
+<!-- アイデアを入力、送信ボタン -->
+<div class="idea-text">
 
+</div>
+<div class="idea-add">
+    <button>+</button>
+</div>
 
 <?php
 require_once __DIR__ . '/../footer.php';
